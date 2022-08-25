f633271ab0963a837396b523dc082cc7dc6f5894
ewoud <ewoudwesterbaan@gmail.com>
2022-08-25 16:42:32 +0200

    Repo georganiseerd

---
+++ README.md
@@ -0,0 +1,12 @@
+build with python 3.10.5
+
+setup:
+python -m venv .venv
+pip install -r requirements
+
+Run hiltermann.py. De output is een excel file met auto's (autos.xlsx).
+
+Er is nog heel veel dat gedaan moet worden:
+- Test toevoegen (en refactoren dat het daadwerklijk testbaar is)
+- Documentatie toevoegen
+- Setup.py (oid) toevoegen
--- src/hiltermann.py
+++ src/hiltermann.py
@@ -1,6 +1,5 @@
 import logging
 from bs4 import BeautifulSoup
-import pprint
 import xlsxwriter
 import requests
 
@@ -10,19 +9,21 @@ def get_content() -> str:
     Get all info from Hiltermann
     """
     logging.info("Getting data from Hiltermann")
-    url = "https://auto.hiltermannlease.nl/configurator/ajax/zoek/uitvoering-list/"
+    url = 'https://auto.hiltermannlease.nl/' \
+          'configurator/ajax/zoek/uitvoering-list/'
 
     headers = {
-    'Accept': '*/*',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'X-Requested-With': 'XMLHttpRequest',
-    'Origin': 'https://auto.hiltermannlease.nl',
-    'Connection': 'keep-alive',
-    'Referer': 'https://auto.hiltermannlease.nl/configurator/zoek/overzicht/',
-    'Sec-Fetch-Dest': 'empty',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Site': 'same-origin'
+        'Accept': '*/*',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Origin': 'https://auto.hiltermannlease.nl',
+        'Connection': 'keep-alive',
+        'Referer': 'https://auto.hiltermannlease.nl/'
+                   'configurator/zoek/overzicht/',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin'
     }
 
     response = requests.request("POST", url, headers=headers)
@@ -35,26 +36,25 @@ def parse_web_result(soup: BeautifulSoup) -> list:
     """
     logging.info('parsing web result')
     result = []
-    for uitvoering_groep in soup.find_all('div', class_ = 'uitvoering-group'):
+    for uitvoering_groep in soup.find_all('div', class_='uitvoering-group'):
         uitvoering_groep_text = uitvoering_groep.find('h2').text
         table = uitvoering_groep.find('table')
-        uitvoeringen = table.find_all('tr', class_ = 'uitvoering')
+        uitvoeringen = table.find_all('tr', class_='uitvoering')
         for uitvoering in uitvoeringen:
             car_rec = {}
             car_rec['uitvoering_groep'] = uitvoering_groep_text
-            a = uitvoering.attrs
             for k, v in uitvoering.attrs.items():
                 if k != 'class':
                     # logging.debug(f"{k}, {v}")
                     car_rec[k] = v
-        
-            uitvoering_cell = uitvoering.find('td', class_ = 'uitvoering-cell')
 
-            info_link = uitvoering_cell.find('a', class_ = 'info-link')
+            uitvoering_cell = uitvoering.find('td', class_='uitvoering-cell')
+
+            info_link = uitvoering_cell.find('a', class_='info-link')
             info_link_href = info_link['href']
             car_rec['info-link'] = info_link_href
 
-            uitvoering_link = uitvoering.find('a', class_ = 'uitvoering-link')
+            uitvoering_link = uitvoering.find('a', class_='uitvoering-link')
             uitvoering_link_href = uitvoering_link['href']
             car_rec['uitvoering-link'] = uitvoering_link_href
 
@@ -64,11 +64,11 @@ def parse_web_result(soup: BeautifulSoup) -> list:
                 car_rec[key] = value
                 # logging.debug(f"{key} - {value}")
 
-            prijs_cell = uitvoering.find('td', class_ = 'prijs-cell')
+            prijs_cell = uitvoering.find('td', class_='prijs-cell')
             prijs = prijs_cell.text
             car_rec['prijs'] = prijs
 
-            lease_prijs_cell = uitvoering.find('td', class_ = 'lease-prijs-cell')
+            lease_prijs_cell = uitvoering.find('td', class_='lease-prijs-cell')
             lease_prijs = lease_prijs_cell.text
             car_rec['lease-prijs'] = lease_prijs
 
@@ -77,8 +77,6 @@ def parse_web_result(soup: BeautifulSoup) -> list:
         # end loop uitvoeringen
     # end loop uitvoeringen-groep
     logging.info(f"Num of records: {len(result)}")
-    pp = pprint.PrettyPrinter(indent=4)
-    # pp.pprint(result)
     return result
 
 
@@ -106,7 +104,8 @@ def write_excel(data: list):
 
 
 def main():
-    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+    logging.basicConfig(format='%(levelname)s:%(message)s',
+                        level=logging.DEBUG)
     content = get_content()
     soup = BeautifulSoup(content, 'html.parser')
     data = parse_web_result(soup)
@@ -114,4 +113,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main() 
+    main()
